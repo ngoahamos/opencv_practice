57151e34ae077a5c6c4197bbfaa63b4e1e45ca20
Amos Ngoah <lifesoftdev@gmail.com>
2023-11-05 23:24:43 +0800

    initial commit

---
+++ video-capture/capture.py
@@ -0,0 +1,20 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+fourcc = cv2.VideoWriter_fourcc(*"XVID")
+out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640,480))
+
+while cap.isOpened():
+    ret,frame = cap.read()
+    if ret == True:
+        out.write(frame)
+        cv2.imshow('Spy Cam', frame)
+        if cv2.waitKey(1) & 0XFF == ord('q'):
+            break
+    else:
+        print('No Capture Device')
+        break
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
