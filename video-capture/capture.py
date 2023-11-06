5d37b65df84bc5ba9d6d17fec9b50cbf85d8c42b
Amos Ngoah <lifesoftdev@gmail.com>
2023-11-06 18:30:32 +0800

    video codec changed to mp4

--- video-capture/capture.py
+++ video-capture/capture.py
@@ -1,8 +1,8 @@
 import cv2
 
 cap = cv2.VideoCapture(0)
-fourcc = cv2.VideoWriter_fourcc(*"XVID")
-out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640,480))
+fourcc = cv2.VideoWriter_fourcc(*"MP4V")
+out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (640,480))
 
 while cap.isOpened():
     ret,frame = cap.read()
